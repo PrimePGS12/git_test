858c5bb2ecc0bcbafde35117936e864b8e2f84ac
PrimePGS12 <147902496+PrimePGS12@users.noreply.github.com>
2023-11-25 12:07:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const app = require("./app");
-const UserModel = require('./modules/user.model')
-const db = require('./config/db')
+const UserModel = require('./modules/user.model'); // Adjust the path accordingly
+const db = require('./config/db');
 
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello World")
-})
+app.get('/', (req, res) => {
+    res.send("Hello Ashrith");
+});
 
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server Listening on Port http://localhost:${port}`);
-})
+});
